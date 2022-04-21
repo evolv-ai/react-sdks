dc62bd79a3aaf619bfbea2d6665690df07caec16
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-04-20 21:28:47 -0300

    feat: evolv-client

---
+++ packages/evolv-client/types/index.ts
@@ -0,0 +1,43 @@
+export interface EvolvClientOptions {
+  enviromentId: string;
+  sessionId?: string;
+  userId?: string;
+}
+
+export interface IExperimentOptions {
+  userId?: string;
+  group: string;
+}
+
+export type IAllocationsResponse = IAllocationsContext[]
+
+export interface IAllocationsContext {
+  genome: IAllocationsGroups
+}
+
+export interface IAllocationsGroups<T = any> {
+  [groupKey: string]: IAllocationsVariants<T>
+}
+
+export interface IAllocationsVariants<T = any> {
+  [variantKey: string]: T
+}
+
+export interface IResponseAlloctaionsVariables {
+  [variableKey: string]: IResponseAllocationsVariants
+}
+
+export interface IResponseAllocationsVariants {
+  [variantKey: string]: IResponseAllocationsVariant
+}
+
+export interface IResponseAllocationsVariant {
+  id: string
+  type: 'compound'
+  _metadata: {}
+  script: string
+  styles: string
+  timing: 'immediate' | string
+  timingSelectors: any
+}
+
