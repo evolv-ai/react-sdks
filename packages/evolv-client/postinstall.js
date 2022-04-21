e7d40741a480b1afd345ed51df4a2d97db1355bf
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-04-20 22:11:27 -0300

    feat: postinstall fix code

---
+++ packages/evolv-client/postinstall.js
@@ -0,0 +1,242 @@
+var fs = require('fs');
+
+function readWriteSync() {
+  var newSrc =
+`
+import MiniPromise from './ponyfills/minipromise.js';
+import base64 from './ponyfills/base64.js';
+import { assign } from './ponyfills/objects.js';
+
+const URL_PATTERN = /^([a-z]+):\\/\\/([^/]+)(.*)/i;
+
+function cryptography() {
+  // eslint-disable-next-line no-undef
+  return typeof crypto !== 'undefined' ? crypto : msCrypto;
+}
+
+/**
+ * Convert a String to an ArrayBuffer
+ *
+ * ie11 Supported
+ *
+ * @param str The String to convert to an ArrayBuffer
+ * @returns {ArrayBuffer} The resulting array buffer encoded as utf-8
+ */
+export function str2ab(str) {
+  if (typeof TextEncoder !== 'undefined') {
+    return (new TextEncoder()).encode(str).buffer;
+  }
+
+  const buf = new ArrayBuffer(str.length);
+  const bufView = new Uint8Array(buf);
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+
+/**
+ * Converts an msCrypto operation to a promise if needed.
+ *
+ * @param op The op (or Promise) to convert if needed.
+ * @returns {PromiseLike} A promise
+ */
+function cryptoOperationToPromise(op) {
+  if (op.then) {
+    return op;
+  }
+
+  return MiniPromise.createPromise(function(resolve, reject) {
+    op.oncomplete = function(evt) {
+      resolve(evt.target.result);
+    };
+
+    function rejectHandler(evt) {
+      reject(evt.toString());
+    }
+    op.onerror = rejectHandler;
+    op.onabort = rejectHandler;
+  });
+}
+
+/**
+ * Sign a String with HMAC-SHA384
+ *
+ * @param {String} key The HMAC key to use for signing
+ * @param {String} payload The String to sign
+ * @returns {PromiseLike<ArrayBuffer>} The cryptographic signature
+ */
+function sign(key, payload) {
+  const keyFormat = 'raw';
+  const algorithm = { name: 'HMAC', hash: 'SHA-384' };
+  const keyUsages = ['sign'];
+  const crypto = cryptography();
+
+  return MiniPromise.createPromise(function(resolve, reject) {
+    cryptoOperationToPromise(crypto.subtle.importKey(keyFormat, str2ab(key), algorithm, true, keyUsages))
+      .then(function (cryptoKey) {
+        cryptoOperationToPromise(crypto.subtle.sign(algorithm, cryptoKey, payload))
+          .then(function(bytes) {
+            resolve(base64.encodeFromArrayBuffer(bytes));
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+}
+
+function createSignatureHeader(signatureKeyId, signature) {
+  return 'keyId="' + signatureKeyId + '",algorithm="hmac-sha384",signature="' + signature + '"';
+}
+
+function xhrRequest(options) {
+  return MiniPromise.createPromise(function(resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', function () {
+      if (this.status >= 400) {
+        reject(this.statusText || ('Evolv: Request failed ' + this.status));
+        return;
+      }
+
+      if (this.status === 200) {
+        resolve(JSON.parse(this.responseText));
+      } else if (this.status === 202) {
+        resolve();
+      } else {
+        const message = 'Evolv: Invalid status ' + this.status + ' for response ' + this.responseText;
+        reject(message);
+      }
+    });
+    xhr.addEventListener('error', reject);
+    xhr.open(options.method, options.url, !options.sync);
+    if (options.method.toUpperCase() === 'POST' || options.method.toUpperCase() === 'PUT') {
+      xhr.setRequestHeader('Content-Type', options.encoding);
+    }
+    xhr.setRequestHeader('Accept', 'application/json');
+    if (options.signature) {
+      xhr.setRequestHeader('Signature', createSignatureHeader(options.keyId, options.signature));
+    }
+    xhr.send(options.payload);
+  });
+}
+
+function nodeRequest(options) {
+  const self = this;
+
+  return MiniPromise.createPromise(function(resolve, reject) {
+    let response = '';
+    const parts = URL_PATTERN.exec(options.url);
+    if (!parts) {
+      throw new Error('Evolv: Invalid endpoint URL');
+    }
+
+    const schema = parts[1];
+    (schema === 'http' ? import('http') : import('https')).then(function (http) {
+      const hostname = parts[2];
+      const path = parts[3];
+      const headers = {
+        'Content-Type': options.encoding,
+        'Accept': 'application/json',
+        // eslint-disable-next-line no-undef
+        'Content-Length': Buffer.byteLength(options.payload)
+      };
+
+      if (options.signature) {
+        headers['Signature'] = createSignatureHeader(options.keyId, options.signature);
+      }
+      const req = http.request({
+        hostname: hostname,
+        path: path,
+        method: options.method,
+        headers: headers
+      }, function (res) {
+        res.on('data', chunk => {
+          response += chunk;
+        });
+        res.on('end', function () {
+          if (res.statusCode === 200) {
+            resolve(JSON.parse(response));
+          } else if (res.statusCode === 202) {
+            resolve();
+          } else {
+            const message = 'Evolv: Invalid status ' + res.statusCode + ' for response ' + res.statusMesssage;
+            reject(message);
+          }
+        });
+      });
+      req.on('error', reject);
+      options.payload && req.write(options.payload);
+      req.end();
+    });
+  });
+}
+
+/**
+ * @typedef RetrieveOptions
+ * @property {string} method
+ * @property {string} url
+ * @property {string} keyId
+ * @property {string} key
+ * @property {object|*} [data]
+ * @property {string} [encoding]
+ */
+
+/**
+ * @param {RetrieveOptions} opts
+ * @param {RequestHooks} [hooks]
+ * @returns {Promise<unknown> | MiniPromise}
+ */
+export default function retrieve(opts, hooks) {
+  const options = (hooks && typeof hooks.beforeOptions === 'function')
+    ? hooks.beforeOptions(opts)
+    : opts;
+
+  return MiniPromise.createPromise(function(resolve, reject) {
+    const completeOptions = assign({}, options);
+    completeOptions.encoding = completeOptions.encoding || 'application/json; charset=UTF-8';
+
+    let payload;
+    if (!completeOptions.data) {
+      payload = '';
+    } else if (typeof completeOptions.data === 'object') {
+      if (completeOptions.encoding === 'application/x-www-form-urlencoded') {
+        payload = Object.keys(completeOptions.data).map(function(key) {
+          return encodeURIComponent(key) + '=' + encodeURIComponent(completeOptions.data[key]);
+        }).join('&');
+      } else {
+        payload = JSON.stringify(completeOptions.data);
+      }
+    } else {
+      payload = completeOptions.data;
+    }
+    completeOptions.payload = payload;
+
+    let rx;
+    if (typeof XMLHttpRequest !== 'undefined') {
+      rx = xhrRequest;
+    } else {
+      rx = nodeRequest;
+    }
+
+    if (!completeOptions.key) {
+      rx(completeOptions)
+        .then(resolve)
+        .catch(reject);
+      return;
+    }
+
+    sign(completeOptions.key, str2ab(completeOptions.payload))
+      .then(function (signature) {
+        rx(assign({signature:signature}, completeOptions))
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+}
+`;
+
+  fs.writeFileSync('./node_modules/@evolv/javascript-sdk/src/retrieve.js', newSrc, 'utf-8');
+}
+
+readWriteSync();
