61adce246ff8d24cf7b1bd5108a3284297685aa0
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-05-23 21:09:57 -0300

    add container option for listenEvent

--- packages/flow/src/triggers/listen-event.ts
+++ packages/flow/src/triggers/listen-event.ts
@@ -1,11 +1,13 @@
 export type ListenEventCallback = (event: Event, element: Element) => void
 export interface ListenEventOptions {
   once?: boolean;
+  container?: Element;
 }
 export type ListenEvent = (event: keyof HTMLElementEventMap, selector: string, callback: ListenEventCallback, options?: ListenEventOptions) => void
 export const listenEvent: ListenEvent = (event, selector, callback, options = {}) => {
   const runOnce = typeof options.once === 'boolean' ? options.once : true;
-  document.body.addEventListener(event, (e) => {
+  const elementToListen = options.container || document.body;
+  elementToListen.addEventListener(event, (e) => {
     const target = e.target as Element;
     if(target.matches(selector)) {
       callback(e, target);
