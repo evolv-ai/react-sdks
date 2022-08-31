41d6d7c4db29d254df5646b052615abe6e353262
Matt Strom <matt.strom@evolv.ai>
2022-08-31 15:41:25 -0700

    feat(AP-1778): add demo sites for nextjs and react

---
+++ examples/nextjs/utils/uid.tsx
@@ -0,0 +1,51 @@
+import { useEvolv } from '@evolv/nextjs';
+import { getCookie, setCookie } from 'cookies-next';
+import { GetServerSidePropsContext } from 'next';
+import { FC, useEffect } from 'react';
+import { generateId } from './generate-id';
+
+
+const UID_COOKIE_KEY = 'evolv:uid';
+const CID_COOKIE_KEY = 'evolv:cid';
+
+export interface UserIdProps {
+	uid: string;
+}
+
+interface Result {
+	props: UserIdProps;
+}
+
+export async function getUserIdProps({ req, res }: GetServerSidePropsContext): Promise<Result> {
+	const isServer = !!req;
+	let uid: string = isServer
+		? req.cookies[UID_COOKIE_KEY]
+		: getCookie(UID_COOKIE_KEY) as string;
+
+	if (!uid) {
+		uid = generateId();
+		setCookie(UID_COOKIE_KEY, uid, { req, res, maxAge: 60 * 6 * 24 });
+	}
+
+	return {
+		props: { uid }
+	};
+}
+
+export function useCidCookie() {
+	const { client, isServer } = useEvolv();
+
+	useEffect(() => {
+		if (!isServer) {
+			client.on('confirmed', () => {
+				const cid = client.context.remoteContext.confirmations?.[0]?.cid;
+				setCookie(CID_COOKIE_KEY, cid, { maxAge: 60 * 6 * 24 });
+			});
+		}
+	}, []);
+}
+
+export const StoreCidCookie: FC = () => {
+	useCidCookie();
+	return null;
+};
