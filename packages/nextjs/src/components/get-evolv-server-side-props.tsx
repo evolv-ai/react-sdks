b05d5e5bd715f3cadfa2d19e335e361122cd79d2
Matt Strom <matt.strom@evolv.ai>
2022-08-31 15:22:10 -0700

    feat(AP-1778): implement nextjs sdk

---
+++ packages/nextjs/src/components/get-evolv-server-side-props.tsx
@@ -0,0 +1,56 @@
+import { EvolvClientOptions } from '@evolv/react';
+import { GetServerSidePropsContext } from 'next';
+import { GetServerSidePropsResult } from 'next/types';
+
+import { ClientAdapter } from '../client.adapter';
+
+
+export interface EvolvOptions {
+	uid: string;
+	client: EvolvClientOptions;
+}
+
+type PropsFactory<T extends GetServerSidePropsResult<any>> = (ctx: GetServerSidePropsContext) => Promise<T>;
+
+export interface EvolvServerSideProps {
+	hydratedState: Record<string, any>;
+}
+
+interface Result {
+	props: EvolvServerSideProps;
+}
+
+/**
+ *
+ * @example
+ * export const getServerSideProps = getEvolvServerSideProps(options);
+ *
+ * @example
+ * export function getServerSideProps(ctx) {
+ *     const { props: evolvProps } = await getEvolvServerSideProps(options, ctx);
+ *     return {
+ *         props: evolvProps
+ *     };
+ * }
+ */
+export function getEvolvServerSideProps(options: EvolvOptions): PropsFactory<Result>;
+export function getEvolvServerSideProps(options: EvolvOptions, ctx: GetServerSidePropsContext): Promise<Result>;
+export function getEvolvServerSideProps(options: EvolvOptions, ctx?: GetServerSidePropsContext): Promise<Result> | PropsFactory<Result> {
+	const factory = async (ctx: GetServerSidePropsContext) => {
+		const client = new ClientAdapter(options.client);
+
+		client.initialize(options.uid);
+
+		await client.getKeys();
+
+		return {
+			props: {
+				hydratedState: client.evolvState
+			}
+		};
+	};
+
+	return ctx
+		? factory(ctx)
+		: factory;
+}
---
+++ packages/nextjs/src/index.ts
@@ -0,0 +1,5 @@
+export { EvolvClient, EvolvClientOptions, useContextState } from '@evolv/react';
+
+export type { ClientAdapter } from './client.adapter';
+export * from './components';
+export * from './hooks';
