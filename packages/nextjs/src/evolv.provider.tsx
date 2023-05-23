72d3eda4f856db09aca08ecda1f5d3c52d3a36df
Matt Strom <5853934+mattstrom@users.noreply.github.com>
2023-05-23 15:10:51 -0700

    fix(AP-2793): add coexistence mode with asset-manager (#40)
    * fix(AP-2793): add coexistence mode with asset-manager
    
    Adds ability for a server-side rendered page, such as one using the NextJS SDK, to indicate that it will be responsible for instantiating the Evolv client
    
    In particular this prevents the occurrence of the following error:
    ```
    TypeError: Cannot set property client of #<Object> which has only a getter
    ```
    
    * docs(AP-2793): add documentation for coexistence mode
    
    * ci(AP-2793): publish using commit from "tag" job

--- packages/nextjs/src/evolv.provider.tsx
+++ packages/nextjs/src/evolv.provider.tsx
@@ -1,16 +1,27 @@
-import { EvolvProvider as ReactEvolvProvider, EvolvProviderProps } from '@evolv/react';
-import React, { FC } from 'react';
+import { ClientAdapter, EvolvProvider as ReactEvolvProvider, EvolvProviderProps } from '@evolv/react';
+import React, { FC, useEffect, useRef } from 'react';
 
 import { getClientName } from './utils/index.js';
 
 
 export const EvolvProvider: FC<EvolvProviderProps> =
-	({ options, ...props }) => (
-		<ReactEvolvProvider
-			{...props}
-			options={{
-				...options,
-				clientName: getClientName()
-			}}
-		/>
-	);
+	({ options, ...props }) => {
+		const ref = useRef<ClientAdapter>();
+
+		useEffect(() => {
+			if (ref.current) {
+				globalThis.evolv.onSsrClientInitialized?.();
+			}
+		}, [ref.current]);
+
+		return (
+			<ReactEvolvProvider
+				{...props}
+				adapterRef={ref}
+				options={{
+					...options,
+					clientName: getClientName()
+				}}
+			/>
+		);
+	};
